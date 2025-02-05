0315ef512b57ec86617a3d896c2f442f6c0d4337
Michelle Cheng <chengmic@oregonstate.edu>
2025-02-04 18:30:21 -0800

    add dice roll total

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,74 @@
 import './App.css'
 import Button from '@mui/material/Button';
+import Grid2 from '@mui/material/Grid2';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 function GenerateNum(min, max) {
-  {/*max is inclusive*/}
+  /*max is inclusive*/
   return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + min);
 }
 
 function App() {
+  const [rolledDice, setRolledDice] = useState([]);
+  const [total, setTotal] = useState(0);
+  
+  useEffect (() => {
+    console.log(rolledDice);
+  }, [rolledDice])
 
-  const handleDieClick = (dieValue) => {
-    return (GenerateNum(1, dieValue));
+  const handleDieButtonClick = (dieSize) => {
+    const roll = (GenerateNum(1, dieSize));
+    console.log("Roll value: " + roll)
+
+    /* die json object */
+    const die = {
+      size: dieSize,
+      value: roll
     }
 
+    setRolledDice([...rolledDice, die])
+    setTotal (total + die.value);
+  }
+
   return (
-    <>
-      <div>
-          <Button onClick={ () => handleDieClick(4)} variant="text">D4</Button>
-          <Button onClick={ () => handleDieClick(6)} classNamevariant="text">D6</Button>
-          <Button onClick={ () => handleDieClick(8)} classNamevariant="text">D8</Button>
-          <Button onClick={ () => handleDieClick(10)} classNamevariant="text">D10</Button>
-          <Button onClick={ () => handleDieClick(12)} classNamevariant="text">D12</Button>
-          <Button onClick={ () => handleDieClick(20)} classNamevariant="text">D20</Button>
-      </div>
-    </>
-  )
+    <div>
+      {/*Dice Tray*/}
+      <Grid2
+        container spacing={2}
+        alignItems="center"
+        justifyContent="center"
+        id="DiceTray"
+        style = {{backgroundColor: "darkblue"}}
+      >
+        This is the Dice Tray.
+      </Grid2>
+
+      {/*Total Display*/}
+      <Grid2
+        id="TotalRoll"
+        style ={{backgroundColor: "gray"}}
+      >
+        Total: {total}
+      </Grid2>
+
+      {/*Dice Selection Buttons*/}
+      <Grid2>
+          <Button onClick={ () => handleDieButtonClick(4)} variant="text">D4</Button>
+          <Button onClick={ () => handleDieButtonClick(6)} variant="text">D6</Button>
+          <Button onClick={ () => handleDieButtonClick(8)} variant="text">D8</Button>
+          <Button onClick={ () => handleDieButtonClick(10)} variant="text">D10</Button>
+          <Button onClick={ () => handleDieButtonClick(12)} variant="text">D12</Button>
+          <Button onClick={ () => handleDieButtonClick(20)} variant="text">D20</Button>
+      </Grid2>
+
+      {/* Roll Button */}
+      <Grid2>
+        <Button id="RollButton" varient="text" style = {{color: "white", backgroundColor: "red"}}>ROLL!</Button>
+      </Grid2>
+    </div>
+  );
 }
 
 
